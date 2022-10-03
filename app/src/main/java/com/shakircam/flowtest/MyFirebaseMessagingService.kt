3a43c533c43f563c5d792e87abb126447e468f8a
shakircam <shakircam@gmail.com>
2022-10-04 01:55:23 +0600

    remove space..

--- app/src/main/java/com/shakircam/flowtest/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/shakircam/flowtest/MyFirebaseMessagingService.kt
@@ -8,8 +8,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
-        Log.d("tag","fcm token1:$token")
     }
 
 
