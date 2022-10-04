59e06a5b071d23b6da494d479070dfc03b95a947
shakircam <shakircam@gmail.com>
2022-10-04 11:03:48 +0600

    delete fcm reference

--- app/src/main/java/com/shakircam/flowtest/MainActivity.kt
+++ app/src/main/java/com/shakircam/flowtest/MainActivity.kt
@@ -2,13 +2,10 @@ package com.shakircam.flowtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.messaging.FirebaseMessaging
 import com.shakircam.flowtest.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -43,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        getToken()
+
         observeData()
 
     }
@@ -82,16 +79,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getToken(){
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener{ task->
-            if (!task.isSuccessful){
-                Log.w("tag","Fetching fcm token is failed ",task.exception)
-            }else{
-                val token = task.result
-                Log.d("tag","fcm token:$token")
-            }
-        }
-
-    }
 }
